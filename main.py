769ce6467db46d17dce9827460a4517b95dfb56f
Huzi <43390871+HuzaifaPatel@users.noreply.github.com>
2023-05-20 00:51:54 -0400

    delete old files

--- main.py
+++
@@ -1,4 +0,0 @@
-import the_elite_videos
-# import the_elite_ltk_videos
-# import fileExists
-# import retry
--- sqldump.h
+++
@@ -1,7 +0,0 @@
-void create_dir();
-void dead_video_changes();
-void player_color();
-void the_elite_ltk_videos();
-void the_elite_single_segment_videos();
-void the_elite_videos();
-void tutorial_videos();
